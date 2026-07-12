2b2c90c58feccddd1058ebbc0df45785438d5672
&Hearys <60827656+BasedDesi@users.noreply.github.com>
2026-07-13 00:25:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ client = OpenAI(
 class ChatRequest(BaseModel):
     model: str
     messages: list
-    temperature: float = 1.0
+    temperature: float = 0.85
     max_tokens: int = 16384
     stream: bool = False
-    top_p: float = 1.0
+    top_p: float = 0.1
     top_k: float = 16
-    frequency_penalty: float = 0.0
+    frequency_penalty: float = 0.39
     presence_penalty: float = 0.0
     repeatition_penalty: float = 0.17
 
